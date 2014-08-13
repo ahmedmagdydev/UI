7c974d44a551c8b5e7ec50796e310c7ade9086ac
Ahmed Magdy <ahmed.magdy.dev@gmail.com>
2014-08-13 22:42:25 +0200

    search page

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 jQuery(document).ready(function($) {
 	$("#accordion").accordion();
 	 $( ".select" ).selectmenu();
+	 $( ".filter" ).menu({ 
+	 	position: { my: "center top", at: "right-5 top+25" } ,
+	 	icons: {  submenu: "ui-icon-carat-1-s" }
+	 });
+	 $("section.result").append('<div class="divider"></div>');
 });
