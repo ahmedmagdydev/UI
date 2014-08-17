324caa562dc43ef141b5f508b9c5d38e06c350d4
Ahmed Magdy <ahmed.magdy.dev@gmail.com>
2014-08-17 20:29:20 +0200

    new message and compose

--- js/main.js
+++ js/main.js
@@ -1,27 +1,36 @@
 jQuery(document).ready(function($) {
 	var article 	= $(".center-article"),
-		leftBar		= $(".left-side-bar")
-		maxHeight	= $(".side-bar").height()-20,
-		options 	= {
-		 	cursorcolor : "#b6b6b6",
-		 	railalign: "left",
-		 	horizrailenabled: false
-		 };;
+		leftBar		= $(".left-side-bar"),
+		store 		= $("article.store"),
+		profile		= $("article.profile"),
+		options 	=  {cursorcolor : "#b6b6b6",railalign: "left",horizrailenabled: false};
+
+		 // jqueryUi
 	$("#accordion").accordion();
-	 $( ".select" ).selectmenu();
-	 $( ".filter" ).menu({ 
-	 	position: { my: "center top", at: "right-5 top+25" } ,
-	 	icons: {  submenu: "ui-icon-carat-1-s" }
-	 });
-	 $("section.result").append('<div class="divider"></div>');
-	 if (article.height() > maxHeight) {
-		 article.height(maxHeight);
-		 article.niceScroll(options);
-	 };
-	 if (leftBar.height() > maxHeight) {
- 		 leftBar.height(maxHeight);
- 		 leftBar.niceScroll(options);
-	 };
+	$( ".select" ).selectmenu();
+	$( ".filter" ).menu({ 
+		position: { my: "center top", at: "right-40 top+45" } ,
+		icons: {  submenu: "ui-icon-carat-1-s" }
+	});
+
+	 // divider
+	$("section.result").append('<div class="divider"></div>');
+	 
+
+	 	 // niceScroll
+	var myNiceScroll = function  (element) {
+	 	var maxHeight	= $(".side-bar").height()-20;
+	 	if (element.height() > maxHeight) {
+		element.height(maxHeight);
+		element.niceScroll(options);
+	 	};
+	};
+	 // myNiceScroll(article);
+	 // myNiceScroll(leftBar);
+	 // myNiceScroll(store);
+	 // myNiceScroll(profile);
+
+
 	
 
 	 
