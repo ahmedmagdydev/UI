94095875c435874e4eab74d4f8a1d844e1f72381
Ahmed Magdy <ahmed.magdy.dev@gmail.com>
2014-10-26 23:59:26 +0200

    m

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function($) {
 		leftBar		= $(".left-side-bar"),
 		store 		= $("article.store"),
 		profile		= $("article.profile"),
-		options 	=  {cursorcolor : "#b6b6b6",railalign: "left",horizrailenabled: false},
+		options 	=  {cursorcolor : "#b6b6b6",railalign: "left",horizrailenabled: false,cursorborder:0},
 		notif		= $(".notifications");
 
 		 // jqueryUi
@@ -28,6 +28,7 @@ jQuery(document).ready(function($) {
 	 // myNiceScroll(store);
 	 // myNiceScroll(profile);
  	$(".notifications").niceScroll(options);
+ 	$('.ui-accordion-content').css('height'	, '500px').niceScroll({cursorcolor : "#b6b6b6",railalign: "left",horizrailenabled: false,cursorborder:0});
  	// $(".side-bar.store").niceScroll(options);
 
  	$('.left-side-bar').scrollToFixed({
@@ -69,4 +70,74 @@ jQuery(document).ready(function($) {
  			pop.removeClass('active').find('span').html(--popNumber);
  		};
  	});
+
+
+ 	// messages view message 
+
+ 	$('.messages td').on('click', '.messageTitle', function(event) {
+ 		event.preventDefault();
+ 		$(this).next('p').toggleClass('show',500);
+ 		
+ 	});
+
+ 	// Change login background each refresh
+	var totalCount = 4;
+	function ChangeIt(){
+		var num = Math.ceil( Math.random() * totalCount );
+		var loginbg = 'url(img/background/login'+num+'.jpg)';
+		$('body.login').css({
+			'background':loginbg,
+			'background-size': 'cover',
+			'background-position': 'top center'
+		});
+	};
+	ChangeIt() ;
+
+	// textarea auto sizing
+	$('textarea').autosize(); 
+
+
+
+	 // Switch Click
+        $('.Switch').click(function() {
+            // Check If Enabled (Has 'On' Class)
+            if ($(this).hasClass('On')){
+                // Try To Find Checkbox Within Parent Div, And Check It
+                $(this).parent().find('input:checkbox').attr('checked', true);
+                // Change Button Style - Remove On Class, Add Off Class
+                $(this).removeClass('On').addClass('Off');
+            } else { // If Button Is Disabled (Has 'Off' Class)
+                // Try To Find Checkbox Within Parent Div, And Uncheck It
+                $(this).parent().find('input:checkbox').attr('checked', false);
+                // Change Button Style - Remove Off Class, Add On Class
+                $(this).removeClass('Off').addClass('On'); 
+            }  
+        });
+        // Loops Through Each Toggle Switch On Page
+        $('.Switch').each(function() {
+            // Search of a checkbox within the parent
+            if ($(this).parent().find('input:checkbox').length){
+ 
+                // This just hides all Toggle Switch Checkboxs
+                // Uncomment line below to hide all checkbox's after Toggle Switch is Found
+                // $(this).parent().find('input:checkbox').hide();
+ 
+                // For Demo, Allow showing of checkbox's with the 'show' class
+                // If checkbox doesnt have the show class then hide it
+                if (!$(this).parent().find('input:checkbox').hasClass("show")){
+                    $(this).parent().find('input:checkbox').hide(); }
+                // Comment / Delete out the above 2 lines when using this on a real site
+ 
+                // Look at the checkbox's checkked state
+                if ($(this).parent().find('input:checkbox').is(':checked')){
+                    // Checkbox is not checked, Remove the On Class and Add the Off Class
+                    $(this).removeClass('On').addClass('Off');
+                } else {           
+                    // Checkbox Is Checked Remove Off Class, and Add the On Class
+                    $(this).removeClass('Off').addClass('On');
+                }
+            }
+        });
+
+
 });
